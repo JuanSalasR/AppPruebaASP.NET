df70318c5ae32ae2b30b99cd84b2ea9dc8886d5e
JuanSalasR <juansalas2627@gmail.com>
2015-03-02 18:57:53 -0500

    hoy

---
+++ App/Modelo/Personas.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace App.Modelo
+{
+    public class Personas
+    {
+         private String id;
+         private String nombre;
+         private String direccion;
+         private String telefono;
+         private String email;
+  
+  public Personas (String i, String n, String d, String t, String e){
+    
+      this.id = i;
+      this.nombre = n;
+      this.direccion = d;
+      this.telefono = t;
+      this.email = e;
+        }
+
+        public String toString()
+        {
+            return null;
+        }
+
+        public String nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+    }
+}
